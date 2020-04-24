0b6bd3ad8c8bdadac2da24841fd2e59d6c6ceae6
Ahmed Essam Mohamed <aessam72@gmail.com>
2020-04-25 01:00:25 +0200

    Update State.java

--- src/com/company/State.java
+++ src/com/company/State.java
@@ -6,11 +6,15 @@ import java.util.HashSet;
 
 public class State {
 
-    private ArrayList<Integer> puzzleState = new ArrayList<Integer>();
+    private ArrayList<Integer> puzzleState;
 
     private ArrayList<State> neighbours = new ArrayList<>();
 
     private int blankPosition;
+
+    private int cost;
+
+    private int heuristicCost;
 
     private static HashSet<Integer> right = new HashSet<Integer>() {{
         add(2);
@@ -18,63 +22,100 @@ public class State {
         add(8);
     }};
 
-    public State(ArrayList<Integer> state, int blankPosition) {
+    public State(ArrayList<Integer> state, int blankPosition, int cost) {
         puzzleState = state;
         this.blankPosition = blankPosition;
+        this.cost = cost;
+    }
+
+    public int getCost() { return this.cost; }
+
+    public void setCost(int cost) { this.cost = cost; }
+
+    public int getHeuristicCost() { return this.heuristicCost; }
+
+    /* f(n) = g(n) + h(n) */
+    public int getPathCost() { return this.cost + this.heuristicCost; }
+
+    public void setHeuristicCost(int cost) { this.heuristicCost = cost; }
+
+    public void computeCost(String heuristic)
+    {
+        int cost = 0;
+
+        if (heuristic.equalsIgnoreCase("euclidean"))
+        {
+            cost = Heuristics.euclideanDistance(this, 3);
+        }
+
+        else if (heuristic.equalsIgnoreCase("manhattan"))
+        {
+            cost = Heuristics.manhattanDistance(this, 3);
+        }
+
+        this.heuristicCost = cost;
     }
 
     public ArrayList<Integer> getPuzzleState() { return puzzleState; }
 
     public ArrayList<State> getNeighbours() { return neighbours; }
 
-    // moveLeft
+    /* moveLeft */
     public State moveLeft(State currentState) {
 
-        /* Moving the blank to the left */
+        /* Moving the blank left */
         int newBlankPosition = currentState.blankPosition - 1;
 
-        State state = new State(currentState.puzzleState, newBlankPosition);
+        ArrayList<Integer> puzzleState = new ArrayList<>(currentState.getPuzzleState());
 
-        Collections.swap(state.puzzleState, newBlankPosition, currentState.blankPosition);
+        Collections.swap(puzzleState, newBlankPosition, currentState.blankPosition);
+
+        State state = new State(puzzleState, newBlankPosition, currentState.cost + 1);
 
         return state;
     }
 
-    // moveRight
+    /* moveRight */
     public State moveRight(State currentState) {
 
-        /* Moving the blank to the left */
+        /* Moving the blank right */
         int newBlankPosition = currentState.blankPosition + 1;
 
-        State state = new State(currentState.puzzleState, newBlankPosition);
+        ArrayList<Integer> puzzleState = new ArrayList<>(currentState.getPuzzleState());
 
-        Collections.swap(state.puzzleState, newBlankPosition, currentState.blankPosition);
+        Collections.swap(puzzleState, newBlankPosition, currentState.blankPosition);
+
+        State state = new State(puzzleState, newBlankPosition, currentState.cost + 1);
 
         return state;
     }
 
-    // moveDown
+    /* moveDown */
     public State moveDown(State currentState) {
 
-        /* Moving the blank to the left */
+        /* Moving the blank down */
         int newBlankPosition = currentState.blankPosition + 3;
 
-        State state = new State(currentState.puzzleState, newBlankPosition);
+        ArrayList<Integer> puzzleState = new ArrayList<>(currentState.getPuzzleState());
 
-        Collections.swap(state.puzzleState, newBlankPosition, currentState.blankPosition);
+        Collections.swap(puzzleState, newBlankPosition, currentState.blankPosition);
+
+        State state = new State(puzzleState, newBlankPosition, currentState.cost + 1);
 
         return state;
     }
 
-    // moveUp
+    /* moveUp */
     public State moveUp(State currentState) {
 
-        /* Moving the blank to the left */
+        /* Moving the blank up */
         int newBlankPosition = currentState.blankPosition - 3;
 
-        State state = new State(currentState.puzzleState, newBlankPosition);
+        ArrayList<Integer> puzzleState = new ArrayList<>(currentState.getPuzzleState());
 
-        Collections.swap(state.puzzleState, newBlankPosition, currentState.blankPosition);
+        Collections.swap(puzzleState, newBlankPosition, currentState.blankPosition);
+
+        State state = new State(puzzleState, newBlankPosition, currentState.cost + 1);
 
         return state;
     }
@@ -85,28 +126,24 @@ public class State {
         /* Left */
         if (this.blankPosition % 3 != 0 && this.blankPosition - 1 >= 0)
         {
-
             this.neighbours.add(moveLeft(this));
         }
 
         /* Up */
         if(this.blankPosition > 2 && this.blankPosition - 3 >= 0)
         {
-
             this.neighbours.add(moveUp(this));
         }
 
         /* Down */
         if(this.blankPosition < 6 && this.blankPosition + 3 <= this.puzzleState.size() - 1)
         {
-
             this.neighbours.add(moveDown(this));
         }
 
         /* Right */
         if(!right.contains(this.blankPosition) && this.blankPosition + 1 <= this.puzzleState.size() - 1)
         {
-
             this.neighbours.add(moveRight(this));
         }
     }
