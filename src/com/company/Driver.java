517ad1c6d7306032304940abb1d49ae63c586740
ahmedessam100 <aessam72@gmail.com>
2020-05-09 16:08:09 +0200

    Fully tested

--- src/com/company/Driver.java
+++ src/com/company/Driver.java
@@ -6,30 +6,21 @@ public class Driver {
     public static void main(String[] args) throws InterruptedException {
 
         Solver solver = new Solver();
-        ArrayList<Integer> initialState = new ArrayList<Integer>(Arrays.asList(1, 2, 5, 3, 0, 4, 6, 7, 8));
 
-    /*
-        ArrayList<Integer> initialState = new ArrayList<Integer>();
-//        ArrayList<Integer> initialState = new ArrayList<Integer>(Arrays.asList(1, 2, 3, 0, 4, 5, 6, 7, 8));
+        ArrayList<Integer> initialState = new ArrayList<>();
 
-
+        /* Generating initial random state */
         for(int i = 0; i < 9; i++) {
             initialState.add(i);
         }
 
         Collections.shuffle(initialState);
 
-    */
 
         String heuristic = "manhattan";
         String searchApproach = "A*";
 
         System.out.println("*************Solving using " + searchApproach + " algorithm*************");
-        long start = System.currentTimeMillis();
         solver.solve(initialState, searchApproach, heuristic);
-        long end = System.currentTimeMillis();
-        float sec = (end - start) / 1000F;
-        System.out.println(sec + " seconds");
-
     }
 }
